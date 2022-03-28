900db006adc536ac86854d92e2c0d4893764181c
JiHo <rlagh8515@gmail.com>
2022-03-28 11:41:05 +0900

    web_craper

---
+++ web_craper/indeed.py
@@ -0,0 +1,32 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l&fromage=any&limit={LIMIT}"
+
+
+def extract_indeed_pages() -> int:
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    pagination = soup.find("div", {"class": "pagination"})
+    links = pagination.find_all('a')
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.string))
+    max_page = pages[-1]
+    return max_page
+
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, 'html.parser')
+    print(result.status_code)
+    results = soup.find_all("a",{"class":"tapItem"})
+    for result in results:
+        title = result.find("h2", {"class":"jobTitle"}).text
+        company = result.find("span", {"class":"companyName"}).text
+        print(company)
+    return jobs
